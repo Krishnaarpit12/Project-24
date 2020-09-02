4833169ce6ca0ed2d2bd3aded53ccfb862cc5da3
Krishnaarpit12 <67108304+Krishnaarpit12@users.noreply.github.com>
2020-09-02 20:47:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,23 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var ground,object1,object2,object3,ball;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var paper;
-var dustbin;
-
-function preload()
-{
-	
-}
-
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1200, 1000);
 
-	
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	paper = new Paper(200,680,10,10);
-	World.add(world,paper);
+	ground = new Ground(600,500,1200,10);
+	object1 = new Dustbin(800,490,200,10);
+	object2 = new Dustbin(700,445,10,100);
+	object3 = new Dustbin(900,445,10,100);
+	ball = new Paper(200,445,20,20);
+   
 
 	Engine.run(engine);
   
@@ -33,9 +27,19 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+  ground.display();
+  object1.display();
+  object2.display();
+  object3.display();
+  ball.display();
   
-  drawSprites();
  
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:420,y:-900})
+	}
 }
 
 
